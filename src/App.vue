f968c42852f169838935a7e0386af04799fbd437
Beto Cuevas <46721097+efturtle@users.noreply.github.com>
2022-03-08 15:40:46 -0600

    componente 9 & 10
    termino del video 10 y comienzo del 9

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <script setup>
-//AQUI NADA MAS IMPORTAMOS COMPONENTES
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+
 import HelloWorld from './components/HelloWorld.vue';
-import victor from './components/victor.vue';
 import InvertirInput from './components/InvertirInput.vue';
-import siuuuu from './components/siuuuu.vue';
+import EmitFunction from './components/EmitFunction.vue';
+import Video9 from './components/video9.vue';
+
 </script>
 
 <template>
-  <victor/>
-  <siuuuu/>
-  <InvertirInput/>
-
+    <EmitFunction/>
+    <Video9/>
 
 </template>
 
---
+++ src/components/Modal.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div>
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+                <div class="box">
+                    Manny se la come
+                </div>
+            </div>
+            <button @click="$emit('close')" class="modal-close is-large" aria-label="close"></button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
